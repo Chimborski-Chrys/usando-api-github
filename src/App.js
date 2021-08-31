2032f820a904e3ba66171df8db0655752218bfbf
Chrystiomar <chrystiomar@hotmail.com>
2021-08-31 00:17:55 -0300

    criando primeiros styled

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import React from 'react';
 import Layout from './components/layout';
+import Profile from './components/profile';
+import { ResetCSS } from './global/resetCSS';
 
-function App() {
+const  App = () => {
   return (
-    <main >
+    <main>
+        <ResetCSS/>
       <Layout>
-        <div>
-          <img src="https://avatars.githubusercontent.com/u/86447837?v=4" alt="Avatar do usuario" />
-          <h1>Chimborski-Chrys</h1>
-          <h3>Username: </h3>
-          <span>Chrystiomar</span>
-          <div>
-            <div>
-              <h4>Followers</h4>
-              <span>5</span>
-            </div>
-            <div>
-            <h4>Starreds</h4>
-              <span>5</span>
-            </div>
-            <div>
-            <h4>Followings</h4>
-              <span>5</span>
-            </div>
-          </div>
-
-        </div>
+        <Profile/>
         <div>Repositories</div>
         <div>Starreds</div>
       </Layout>
     </main>
   );
-}
+};
 
 export default App;
---
+++ src/components/profile/styled.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    align-items: flex-start;
+    
+`;
+
+export const WrapperInfoUser = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    height: auto;
+    margin-left: 10px;
+    height: 190px;
+    h1{
+        font-size:30px;
+        font-weight: bold;
+    }
+
+    h3{
+        font-size:20px;
+        font-weight: bold;
+    }
+
+    h4{
+        font-size:18px;
+        font-weight: bold;
+    }
+
+`;
+
+export const WrapperStatusCount = styled.div`
+    display: flex;
+    align-items: center;   
+    div{
+        margin:5px;
+        text-align: center;
+    }     
+`;
+
+export const WrapperUserName = styled.div`
+    display: flex;
+    align-items: center; 
+    margin-top: 20px;  
+    h3 {
+        margin-right: 10px;
+    }  
+    a{
+        font-size: 15px;
+        color : brown;
+        font-weight: bold;
+    } 
+`;
+
+export const WrapperImage = styled.img`
+   border-radius:50%;
+   width: 150px;
+   height: 150px;
+   margin: 10px;
+`;
+
+
