9190c9a27b5535ed7d600106dea6448bf6411449
Chrystiomar <chrystiomar@hotmail.com>
2021-09-01 23:49:17 -0300

    Renderizando item

--- src/components/repositories/index.js
+++ src/components/repositories/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import RepositoryItem from '../repository-item';
 import * as S from './styled';
 
 const Repositories = () => {
@@ -11,8 +12,18 @@ const Repositories = () => {
                 <S.WrapperTab>Repositories</S.WrapperTab>     
                 <S.WrapperTab>Starred</S.WrapperTab>       
             </S.WrapperTabList>        
-            <S.WrapperTabPanel>Panel Repositories</S.WrapperTabPanel>
-            <S.WrapperTabPanel>Panel Starred</S.WrapperTabPanel>
+            <S.WrapperTabPanel>
+                <RepositoryItem 
+                    name="bookFlix"
+                    linkToRepo="https://github.com/Chimborski-Chrys/bookFlix"
+                    fullName="Chimborski-Chrys/bookFlix"/>
+            </S.WrapperTabPanel>
+            <S.WrapperTabPanel>
+                <RepositoryItem 
+                    name="BuscaRestaurante"
+                    linkToRepo="https://github.com/Chimborski-Chrys/BuscaRestaurante"
+                    fullName="Chimborski-Chrys/BuscaRestaurante"/>
+            </S.WrapperTabPanel>
         </S.WrapperTabs>
     );
 }
--- src/components/repository-item/index.js
+++ src/components/repository-item/index.js
@@ -1,17 +1,13 @@
+import React from 'react';
 
-import React from "react";
-import * as S from "./styled";
-
-const RepositoryItem = ({ name, linkToRepo, fullName }) => {
-  return (
-    <S.Wrapper>
-      <S.WrapperTitle>{name}</S.WrapperTitle>
-      <S.WrapperFullName>full name:</S.WrapperFullName>
-      <S.WrapperLink href={linkToRepo} target="_blank" rel="noreferrer">
-        {fullName}
-      </S.WrapperLink>
-    </S.Wrapper>
-  );
+const RepositoryItem = ({name,linkToRepo,fullName }) => {
+    return (
+        <div>
+            <h2>{name}</h2>
+            <h4>full_name</h4>
+            <a href={linkToRepo} target="_blank" rel="noreferrer">{fullName}</a>
+        </div>
+    );
 };
 
 export default RepositoryItem;
